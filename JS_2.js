31a168b76d0bee8fe2d45057c23e3f284a3a6221
Valetina-2608 <valusha2608@gmail.com>
2022-08-08 12:11:04 +0300

    Add solution for task2

---
+++ JS_2.js
@@ -0,0 +1,19 @@
+﻿
+/* Create a function take(fn, count) 
+that calls the function fn the specified number (count) 
+times and returns an array with the results of the calls.
+*/
+
+function take(fn, count) {
+let res=[];
+for(let i=0;i<count;i++){
+res[i]=fn();
+}
+return res;
+}
+
+
+
+
+
+
