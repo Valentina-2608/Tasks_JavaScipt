78a35841a984f21daa094831576e5553f0826b4a
Valetina-2608 <valusha2608@gmail.com>
2022-08-08 12:36:30 +0300

    Add solution for task8

---
+++ JS_8.js
@@ -0,0 +1,26 @@
+﻿
+/* Write a pluck function that takes an array of objects and returns an array of values ​​for a specific field.
+
+var characters = [
+  { 'name': 'barney', 'age': 36 },
+  { 'name': 'fred', 'age': 40 }
+];
+
+*/
+
+
+function pluck(objects, fieldName) {
+let res=[];
+for(let elem of objects){
+	res.push(elem[fieldName]);
+}
+return res;
+};
+
+
+console.log(pluck(characters,'name'));
+
+
+
+
+
