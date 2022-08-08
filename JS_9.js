18fe5e1484d904d8941d41efd6052bad99369c34
Valetina-2608 <valusha2608@gmail.com>
2022-08-08 12:40:29 +0300

    Add solution for task9

---
+++ JS_9.js
@@ -0,0 +1,28 @@
+﻿
+
+/* Write a filter() function that takes a predicate function and an array.
+ It returns an array of values ​​for which the predicate will return true.
+
+*/
+
+let numbers=[1,2,3,4,5,6,7,8,9,10];
+function isEven(x){
+	return x%2==0;
+}
+function filter(arr,fn){
+let res=[];
+for(let elem of arr){
+	if (fn(elem)){
+		res.push(elem);	
+}
+}
+return res;
+}
+
+console.log(filter(numbers,isEven));//result:2,4,6,8,10
+
+
+
+
+
+
