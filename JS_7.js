85de694ccd70504eface1fc64db92d59b1ec4dbe
Valetina-2608 <valusha2608@gmail.com>
2022-08-08 12:29:58 +0300

    Add solution for task7

---
+++ JS_7.js
@@ -0,0 +1,20 @@
+﻿
+/* Write a bind function that allows you to bind a context (this value) to a function.*/
+
+window.x = 1;
+var ctx = { x: 2 };
+function testThis(a){ 
+console.log("x=" + this.x + ", a=" + a);
+}
+
+function bind(fn,context){
+	return fn.bind(context);
+}
+var boundFunction=bind(testThis,ctx);
+console.log(boundFunction(100));
+
+
+
+
+
+
