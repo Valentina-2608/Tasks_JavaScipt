68982f26df79b2d7ec85804c06a538ca6b861534
Valetina-2608 <valusha2608@gmail.com>
2022-08-08 12:22:03 +0300

    Add solution for task5

---
+++ JS_5.js
@@ -0,0 +1,16 @@
+﻿
+/* Partial application
+
+Write a partial(fn, a1, a2, ....) function that allows you to fix one or more function arguments.
+*/
+
+
+function partial(fn,...arg1){
+	return function(...arg2){
+		return fn(...arg1,...arg2);
+	};
+};
+
+
+
+
