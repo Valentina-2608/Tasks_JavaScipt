c2364098fdb6ff33f3c610319435a3255b3ecc15
Valetina-2608 <valusha2608@gmail.com>
2022-08-08 12:24:32 +0300

    Add solution for task6

---
+++ JS_6.js
@@ -0,0 +1,32 @@
+﻿
+/* 
+Our partial function only allows us to capture the first arguments. 
+Improve it so that any arguments can be captured, missing arguments are denoted with undefined.
+*/
+
+
+function partialAny(fn) {
+  let args = Array.prototype.slice.call(arguments, 1);
+  let unArr = [];
+  for (i = 0; i < args.length; i++) {
+    if (args[i] === undefined) unArr.push(i);
+  }
+  return function () {
+    let j = 0;
+    let args1 = Array.prototype.slice.call(arguments, 0);
+    if (unArr.length > 0) {
+      for (i = 0; i < unArr.length; i++) {
+        args[unArr[i]] = args1[j];
+        j++;
+      }
+    }
+    for (i = 0; i < args1.length - unArr.length; i++) {
+      args.push(args1[j]);
+      j++;
+    }
+    return fn.apply(null, args);
+  };
+}
+
+
+
