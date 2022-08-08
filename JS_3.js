1cbb7f9272f685ef5bb05a5424cfeb71469d45e8
Valetina-2608 <valusha2608@gmail.com>
2022-08-08 12:16:41 +0300

    Add solution for task3

---
+++ JS_3.js
@@ -0,0 +1,26 @@
+﻿
+/* Write a function map(fn, array), such a function and an array as input,
+and processes each element of the array with this function, returning a new array.
+*/
+
+
+
+let numbers=[1,2,3,4,5];
+function square(x){
+	return x*x;
+}
+
+function map(fn,arr){
+let res=[];
+for(let elem of arr){
+	res.push(fn(elem));	
+}
+return res;
+}
+console.log(map(square,numbers));//result: 1,4,9,16,25
+
+
+
+
+
+
